4f1a78edf5ca3547b26c5d0c15dafcf8822c4172
shoebilyas123 <shoebilyas123@gmail.com>
2024-05-11 15:50:24 +0530

    signalling server init

---
+++ constants/events.ts
@@ -0,0 +1,7 @@
+export const CALL_ACCEPTED = 'call:accepted';
+export const CALL_USER = 'call:user';
+export const INCOMING_CALL = 'incoming:user';
+export const PEER_NEG_NEEDED = 'peer:negotation-needed';
+export const PEER_NEG_DONE = 'peer:negotiation-done';
+export const JOIN_ROOM = 'room:join:request';
+export const NEW_USER_JOINED = 'room:user:joined';
---
+++ index.ts
@@ -0,0 +1,64 @@
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import dotenv from 'dotenv';
+import * as app_consts from './constants/events.js';
+
+dotenv.config({});
+
+const _httpServer = createServer();
+
+const io = new Server(_httpServer, {
+  cors: {
+    origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
+    methods: process.env.CORS_METHODS.split('/') || ['GET', 'POST'],
+  },
+});
+
+const {
+  CALL_ACCEPTED,
+  CALL_USER,
+  INCOMING_CALL,
+  PEER_NEG_NEEDED,
+  PEER_NEG_DONE,
+  JOIN_ROOM,
+  NEW_USER_JOINED,
+} = app_consts;
+
+const aliasToRoomMap = new Map<string, string>();
+
+io.on('connection', (socket) => {
+  console.log(`SOCKET CONNECTED: ${socket.id}`);
+
+  socket.on(JOIN_ROOM, ({ roomNumber, alias }) => {
+    console.log({ roomNumber, alias, EM: aliasToRoomMap.get(alias) });
+    if (aliasToRoomMap.has(alias)) {
+      socket.leave(aliasToRoomMap.get(alias));
+    }
+    socket.join(roomNumber);
+    io.to(roomNumber).emit(NEW_USER_JOINED, {
+      remoteId: socket.id,
+      remoteAlias: alias,
+    });
+    aliasToRoomMap.set(alias, roomNumber);
+    io.to(socket.id).emit(JOIN_ROOM, { roomNumber, alias });
+  });
+
+  // User calls remote (with Offer)
+  socket.on(CALL_USER, ({ to, offer }) => {
+    io.to(to).emit(INCOMING_CALL, { offer, from: socket.id });
+  });
+  socket.on(CALL_ACCEPTED, ({ to, answer }) => {
+    console.log(answer);
+    io.to(to).emit(CALL_ACCEPTED, { from: socket.id, answer });
+  });
+  socket.on(PEER_NEG_NEEDED, ({ to, offer }) => {
+    io.to(to).emit(PEER_NEG_NEEDED, { from: socket.id, offer });
+  });
+  socket.on(PEER_NEG_DONE, ({ to, offer }) => {
+    io.to(to).emit(PEER_NEG_DONE, { from: socket.id, offer });
+  });
+});
+
+_httpServer.listen(8000, () => {
+  console.log(`SigServer Running on PORT:${process.env.PORT || 8000}`);
+});
